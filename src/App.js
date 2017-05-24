46665b4aa83593a9bc81cf4189c0eee72afd70a4
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-24 14:57:08 -0700

    Project scaffolding

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        <h2>CPDB: Cute Pomeranian Database</h2>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
---
+++ src/components/ThumbnailImage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ThumbnailImage extends Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
