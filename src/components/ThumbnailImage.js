e3856487f98f61c97410a19c4ed4619884390095
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-24 21:01:07 -0700

    Added SCSS for thumbnail gallery

--- src/components/ThumbnailImage.js
+++ src/components/ThumbnailImage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/ThumbnailImage.css';
 // float or inline-block list - show "cards" for each image
 // each image card shows a "thumbnail" (~ 100x100 pixel scale) and the title (above or below).
 
 export default function ThumbnailImage({ name, description, url }) {
   return (
-    <div>
+    <div className="thumbnail">
       <img src={url} alt={'A pom'} />
       <h2>{name}</h2>
     </div>
