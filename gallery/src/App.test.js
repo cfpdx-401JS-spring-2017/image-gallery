58d38ab21f7dbb9b2e4aac76f8689b23c0e436cf
Chris_Wallace <kawallis@icloud.com>
2017-05-25 09:12:18 -0700

    changing

--- gallery/src/App.test.js
+++ gallery/src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Gallery from './Gallery';
+import ThumbNail from './ThumbNail';
+import List from './List';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders a snapshot of main App component', () => {
+  const tree = renderer.create(<App/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders a snapshot of Gallery component', () => {
+  const bobo = renderer.create(<Gallery />).toJSON();
+  expect(bobo).toMatchSnapshot();
+});
+
+
