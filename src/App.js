849171053a21531ab4386c05b017390249c253cf
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-26 14:57:05 -0700

    Adding Pom button working

--- src/App.js
+++ src/App.js
@@ -11,11 +11,32 @@ class App extends Component {
     this.state = {
       poms: null
     };
+
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
     pomsAPI.get()
       .then(poms => this.setState({ poms }));
+  }
+
+  handleAdd(newPom) {
+    pomsAPI.addPom(newPom)
+      .then(pom => {
+        this.setState({
+          poms: [...this.state.poms, pom]
+        });
+      });
+  }
+
+  handleDelete(id, index) {
+    pomsAPI.deletePom(id)
+      .then(() => {
+        const poms = this.state.poms.slice();
+        poms.splice(index, 1);
+        this.setState({ poms });
+      });
   }
 
   render() {
@@ -26,7 +47,7 @@ class App extends Component {
           Welcome to the <code>Cute Pomeranian Database</code>! Check out our poms by clicking below:
         </p>
         {this.state.poms && <Wrapper poms={this.state.poms} />}
-        <AddFile poms={this.state.poms} />
+        <AddFile poms={this.state.poms} onAdd={this.handleAdd} />
       </div>
     );
   }
--- src/components/AddFile.js
+++ src/components/AddFile.js
@@ -3,12 +3,64 @@ import React, { Component } from 'react';
 export default class AddFile extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      name: '',
+      description: '',
+      url: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  reset() {
+    this.setState({
+      name: '',
+      description: '',
+      url: ''
+    });
+  }
+
+  handleChange(event) {
+    const { name, description, url, value } = event.target;
+    this.setState({
+      [name]: value,
+      [description]: value,
+      [url]: value
+    });
   }
 
   render() {
-    return(
+    const { name, description, url } = this.state;
+
+    return (
       <div>
-        {}
+        <h2>Like Poms? Add Your Own!</h2>
+        <form onSubmit={e => {
+          e.preventDefault();
+          this.props.onAdd(this.state);
+          this.reset();
+        }}>
+          <label>
+            name: <input name="name"
+              required
+              value={name}
+              onChange={this.handleChange} />
+          </label>
+          <label>
+            url: <input name="url"
+              required
+              value={url}
+              onChange={this.handleChange} />
+          </label>
+          <label>
+            description: <input name="description"
+              required
+              value={description}
+              onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
