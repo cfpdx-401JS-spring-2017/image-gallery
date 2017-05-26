8724e5ff10be6fca7b431a262dd09407884cab11
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-26 15:28:22 -0700

    Buttons now working and visible for delete

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -10,12 +10,13 @@ GalleryImage.propTypes = {
   })
 };
 
-export default function GalleryImage({ poms }) {
+export default function GalleryImage({ poms, onDelete }) {
   return (
     <div className="gallery">
       <img src={poms.url} alt={'A pom'} />
       <h2>{poms.name}</h2>
       <p>{poms.description}</p>
+      <button onClick={onDelete}>Delete</button>
     </div>
   );
 }
