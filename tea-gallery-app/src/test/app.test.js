df6566f76d3d343e87b2d2975fd2a93101d13fed
nicky evers <livelyevers@gmail.com>
2017-05-26 11:32:49 -0700

    added test

--- tea-gallery-app/src/test/app.test.js
+++ tea-gallery-app/src/test/app.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import GalleryItem from '../components/GalleryItem';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const images = [
+  {
+    title: 'tea leaves in teapot',
+    description: 'what nice leaves',
+    url: '/../public/img/teainpot.jpg'
+  },
+  {
+    title: 'Tetsubin over charcoal',
+    description: 'what a nice tetsubin',
+    url: '/../public/img/tetsubin-charcoal.jpg'
+  },
+  {
+    title: 'Teapot display',
+    description: 'what a nice teapot display',
+    url: '/../public/img/teapot-display.jpg'
+  }
+];
+
+describe('Gallery Images', () => {
+  it('pulls up gallery view', () => {
+    const component = renderer.create(<GalleryItem images={images} />);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+
 });
