c566100713e67564928d4c205edb209ed47c5c3b
nicky evers <livelyevers@gmail.com>
2017-05-31 12:19:45 -0700

    changed image data into an array of album objects

--- tea-gallery-app/src/data.js
+++ tea-gallery-app/src/data.js
@@ -2,52 +2,70 @@ import shortId from 'shortid';
 
 const images = [
   {
-    title: 'tea leaves in teapot',
-    description: 'what nice leaves',
-    url: '/img/teainpot.jpg',
-    _id: shortId.generate()
+    _id: 123,
+    name: 'Teaware',
+    images: [
+      {
+        title: 'tea leaves in teapot',
+        description: 'what nice leaves',
+        url: '/img/teainpot.jpg',
+        _id: shortId.generate()
+      },
+      {
+        title: 'Tetsubin over charcoal',
+        description: 'what a nice tetsubin',
+        url: '/img/tetsubin-charcoal.jpg',
+        _id: shortId.generate()
+      },
+      {
+        title: 'Teapot display',
+        description: 'what a nice teapot display',
+        url: '/img/teapot-display.jpg',
+        _id: shortId.generate()
+      }
+    ]
   },
   {
-    title: 'Tetsubin over charcoal',
-    description: 'what a nice tetsubin',
-    url: '/img/tetsubin-charcoal.jpg',
-    _id: shortId.generate()
+    _id: 456,
+    name: 'Tea Plants',
+    images: [
+      {
+        title: 'Tealeaf upclose',
+        description: '',
+        url: '/img/tealeaf-upclose.jpg',
+        _id: shortId.generate()
+      },
+      {
+        title: 'Da Hong Pao',
+        description: '',
+        url: '/img/da-hong-pao.jpg',
+        _id: shortId.generate()
+      },
+      {
+        title: 'golden needle',
+        description: '',
+        url: '/img/golden-needle.jpg',
+        _id: shortId.generate()
+      }
+    ]
   },
   {
-    title: 'Teapot display',
-    description: 'what a nice teapot display',
-    url: '/img/teapot-display.jpg',
-    _id: shortId.generate()
-  },
-  {
-    title: 'Tealeaf upclose',
-    description: '',
-    url: '/img/tealeaf-upclose.jpg',
-    _id: shortId.generate()
-  },
-  {
-    title: 'Da Hong Pao',
-    description: '',
-    url: '/img/da-hong-pao.jpg',
-    _id: shortId.generate()
-  },
-  {
-    title: 'golden needle',
-    description: '',
-    url: '/img/golden-needle.jpg',
-    _id: shortId.generate()
-  },
-  {
-    title: 'GongFu pour',
-    description: '',
-    url: '/img/gongfu-pour.jpg',
-    _id: shortId.generate()
-  },
-  {
-    title: 'GongFu pour2',
-    description: '',
-    url: '/img/gongfu-pour2.jpg',
-    _id: shortId.generate()
+    _id: 456,
+    name: 'Tea Spaces',
+    images: [
+      {
+        title: 'GongFu pour',
+        description: '',
+        url: '/img/gongfu-pour.jpg',
+        _id: shortId.generate()
+      },
+      {
+        title: 'GongFu pour2',
+        description: '',
+        url: '/img/gongfu-pour2.jpg',
+        _id: shortId.generate()
+      }
+    ]
   }
 ];
 
