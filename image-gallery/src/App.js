5d0b46c3373374964581ac798f47d98adb500811
Morgan Fogarty <mofo37@gmail.com>
2017-05-31 11:59:28 -0700

    make listItem component

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
+import ListItem from './components/ListItem';
+import images from './data';
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    };
+  };
+
+componentDidMount() {
+  images.get(images => {
+    console.log('images', images);
+    return images;
+  })
+    .then(images => {
+      this.setState({data: images});
+    })
+}
+
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <ListItem images={this.state.data[0]}/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ image-gallery/src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+
+  render() {
+   let {images}= this.props;
+
+   if (!images.length) return <div> loading... </div>
+
+   return(
+     <div className='ListItem'>
+      <h2>{images.title}</h2>
+     </div>
+    );
+  }
+}
+
+export default ListItem;
