66265ec0141ebc29f1bd0fa556035a299e56a3a7
Morgan Fogarty <mofo37@gmail.com>
2017-06-02 11:26:20 -0700

    Made gallery display single image with button click!

--- image-gallery/src/components/gallery.js
+++ image-gallery/src/components/gallery.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      indexTracker: 0
+    }
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+
+  };
+
+  increment() {
+    let current = this.state.indexTracker;
+    if (current === this.props.images.length - 1) {
+      this.setState({ indexTracker: current = 0 })
+    }
+    else {
+      this.setState({ indexTracker: current += 1 })
+    };
+  }
+
+  decrement() {
+    let current = this.state.indexTracker -1;
+    if (current === -1) {
+      this.setState({ indexTracker: current = this.props.images.length -1 })
+    }
+    else {
+      this.setState({ indexTracker: current })
+    };
+  }
 
   render() {
-    return(
+    return (
       <div>
-        <h1>HIIIIIII</h1>
+        <img src={this.props.images[this.state.indexTracker].url} alt={'Riot Grrrl'} />
+        <h1>{this.props.images[this.state.indexTracker].title}</h1>
+        <button onClick={this.decrement}>Back</button>
+        <button onClick={this.increment}>Next</button>
       </div>
     );
   }
 }
+
+Gallery.propTypes = {
+  indexTracker: PropTypes.number
+};
 
 export default Gallery;
