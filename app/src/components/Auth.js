0dc4b51d47c7bd00e4ed8c8fc672053dce914e88
Keeley Hammond <keeleymhammond@gmail.com>
2017-06-08 12:54:29 -0700

    Init for auth

---
+++ app/src/components/Auth.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Router,
+  Switch,
+  Link,
+  withRouter
+} from 'react-router';
+import { connect } from 'react-redux';
+
+const Auth = ({user, signin, signup, error, location }) => {
+  return(
+    <div>
+
+    </div>
+  );
+};
+
