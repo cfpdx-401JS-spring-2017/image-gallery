9674d1feebea7a5d59cb4ae4bc419e744050fef5
Morgan Fogarty <mofo37@gmail.com>
2017-06-20 14:31:48 -0700

    brought back test

--- image-gallery/src/_test_/App.test.js
+++ image-gallery/src/_test_/App.test.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from '../App';
-// import renderer from 'react-test-renderer';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import renderer from 'react-test-renderer';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
 
-// it('renders a snapshot', () => {
-//   const wrapper = renderer.create(<App/>).toJSON(); 
-//   expect(wrapper).toMatchSnapshot();
-// });
+it('renders a snapshot', () => {
+  const wrapper = renderer.create(<App/>).toJSON(); 
+  expect(wrapper).toMatchSnapshot();
+});
